cf751d4371893733c97f0edae85a3b1863f9d9bd
eallic <212250502+eallic@users.noreply.github.com>
2026-02-26 23:16:18 +0200

    Add framebuffer loading

--- bootloader/lib.rs
+++ bootloader/lib.rs
@@ -3,6 +3,24 @@
 extern crate self as bootloader;
 
 use core::arch::asm;
+use x86_64::PhysAddr;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Framebuffer {
+    pub addr: PhysAddr,
+    pub width: usize,
+    pub height: usize,
+    pub stride: usize,
+    pub bpp: usize,
+    pub pixel_format: PixelFormat,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum PixelFormat {
+    Rgb,
+    Bgr,
+}
 
 pub fn hcf() -> ! {
     loop {
--- bootloader/main.rs
+++ bootloader/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use bootloader::Framebuffer;
+use bootloader::PixelFormat;
 use core::panic::PanicInfo;
 use core::slice;
 use uefi::CStr16;
@@ -9,6 +11,9 @@ use uefi::boot::MemoryType;
 use uefi::fs::FileSystem;
 use uefi::mem::memory_map::MemoryMapMut;
 use uefi::prelude::*;
+use uefi::proto::console::gop;
+use uefi::proto::console::gop::GraphicsOutput;
+use x86_64::PhysAddr;
 use xmas_elf::ElfFile;
 
 #[entry]
@@ -18,6 +23,10 @@ fn main() -> Status {
     log::info!("Loading kernel");
     let kernel_bytes = load_file(cstr16!("KERNEL.ELF"));
     let _kernel = ElfFile::new(kernel_bytes).unwrap();
+
+    log::info!("Loading framebuffer");
+    let framebuffer = load_framebuffer();
+    log::info!("Loaded framebuffer: {:#?}", framebuffer);
 
     log::info!("Exiting boot services");
     let mut memory_map = unsafe { boot::exit_boot_services(None) };
@@ -46,6 +55,27 @@ fn load_file(path: &CStr16) -> &'static mut [u8] {
     dst.copy_from_slice(&src);
 
     dst
+}
+
+fn load_framebuffer() -> Framebuffer {
+    let gop_handle = boot::get_handle_for_protocol::<GraphicsOutput>().unwrap();
+    let mut gop = boot::open_protocol_exclusive::<GraphicsOutput>(gop_handle).unwrap();
+
+    let mode_info = gop.current_mode_info();
+    let mut gop_framebuffer = gop.frame_buffer();
+
+    Framebuffer {
+        addr: PhysAddr::new(gop_framebuffer.as_mut_ptr() as u64),
+        width: mode_info.resolution().0,
+        height: mode_info.resolution().1,
+        stride: mode_info.stride(),
+        bpp: 4,
+        pixel_format: match mode_info.pixel_format() {
+            gop::PixelFormat::Rgb => PixelFormat::Rgb,
+            gop::PixelFormat::Bgr => PixelFormat::Bgr,
+            _ => panic!("Unsupported pixel format"),
+        },
+    }
 }
 
 #[panic_handler]
