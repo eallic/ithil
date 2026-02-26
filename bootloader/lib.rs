27b3190699656fe02058f1b0c8b5ddef8c108833
eallic <212250502+eallic@users.noreply.github.com>
2026-02-26 22:53:04 +0200

    Add bootloader file loading

--- bootloader/lib.rs
+++ bootloader/lib.rs
@@ -1,1 +1,11 @@
 #![no_std]
+
+extern crate self as bootloader;
+
+use core::arch::asm;
+
+pub fn hcf() -> ! {
+    loop {
+        unsafe { asm!("cli; hlt") }
+    }
+}
--- bootloader/main.rs
+++ bootloader/main.rs
@@ -1,22 +1,56 @@
 #![no_std]
 #![no_main]
 
-use core::arch::asm;
 use core::panic::PanicInfo;
+use core::slice;
+use uefi::CStr16;
+use uefi::boot::AllocateType;
+use uefi::boot::MemoryType;
+use uefi::fs::FileSystem;
+use uefi::mem::memory_map::MemoryMapMut;
 use uefi::prelude::*;
+use xmas_elf::ElfFile;
 
 #[entry]
 fn main() -> Status {
     uefi::helpers::init().unwrap();
 
-    panic!("test");
+    log::info!("Loading kernel");
+    let kernel_bytes = load_file(cstr16!("KERNEL.ELF"));
+    let _kernel = ElfFile::new(kernel_bytes).unwrap();
+
+    log::info!("Exiting boot services");
+    let mut memory_map = unsafe { boot::exit_boot_services(None) };
+    memory_map.sort();
+
+    bootloader::hcf();
+}
+
+fn load_file(path: &CStr16) -> &'static mut [u8] {
+    let sfs = boot::get_image_file_system(boot::image_handle()).unwrap();
+    let mut fs = FileSystem::new(sfs);
+
+    let file_info = fs.metadata(path).unwrap();
+    let file_size = file_info.file_size() as usize;
+
+    let file_ptr = boot::allocate_pages(
+        AllocateType::AnyPages,
+        MemoryType::LOADER_DATA,
+        file_size.div_ceil(4096),
+    )
+    .unwrap()
+    .as_ptr();
+
+    let src = fs.read(path).unwrap();
+    let dst = unsafe { slice::from_raw_parts_mut(file_ptr, file_size) };
+    dst.copy_from_slice(&src);
+
+    dst
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     log::error!("{info}");
 
-    loop {
-        unsafe { asm!("cli; hlt") }
-    }
+    bootloader::hcf();
 }
